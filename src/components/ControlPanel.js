3afbd6774423f9aec95f4d8789e0605953023303
dbto1970 <donbartojunior@gmail.com>
2021-12-04 14:59:11 -0500

    adding material kit ui

--- src/components/ControlPanel.js
+++ src/components/ControlPanel.js
@@ -1,10 +1,18 @@
 import React from "react";
 
+const source = {
+    url: 'https://youtu.be/Bxs8p1x38YE',
+    displayName: 'YouTube',
+    default: true,
+    favorite: true,
+};
+
 function ControlsPanel () {
 
     return (
         <div className='controls'>
             User Controls Go Here
+
         </div>
     )
 }
